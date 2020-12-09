740f6965fd5a05cb2a7ef8a6fd5eeddbc81401f9
nopara73 <adam.ficsor73@gmail.com>
2020-12-09 18:52:22 +0100

    Change to Descending Powers of 2

---
+++ AmountOrganization/AmountOrganization.AscendingPow2/DescPow2Mixer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmountOrganization.DescPow2
+{
+    internal class DescPow2Mixer : IMixer
+    {
+        public DescPow2Mixer(uint feeRate = 10, uint inputSize = 69, uint outputSize = 33, uint sanityFeeRate = 2, ulong sanityFee = 1024)
+        {
+            DustThreshold = new[] { sanityFee, feeRate * inputSize, sanityFeeRate * inputSize }.Max();
+            FeeRate = feeRate;
+            InputSize = inputSize;
+            OutputSize = outputSize;
+        }
+
+        public ulong DustThreshold { get; }
+        public ulong InputFee => InputSize * FeeRate;
+        public ulong OutputFee => OutputSize * FeeRate;
+
+        public uint FeeRate { get; }
+        public uint InputSize { get; }
+        public uint OutputSize { get; }
+
+        public IEnumerable<IEnumerable<ulong>> CompleteMix(IEnumerable<IEnumerable<ulong>> inputs)
+        {
+            var inputArray = inputs.ToArray();
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                var currentUser = inputArray[i];
+                var others = new List<ulong>();
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        others.AddRange(inputArray[j]);
+                    }
+                }
+                yield return Mix(currentUser, others).Select(x => x - OutputFee);
+            }
+        }
+
+        public IEnumerable<ulong> Mix(IEnumerable<ulong> myInputsParam, IEnumerable<ulong> othersInputsParam)
+        {
+            var myInputs = myInputsParam.Select(x => x - InputFee).ToArray();
+            var othersInputs = othersInputsParam.Select(x => x - InputFee).ToArray();
+            var largestOthersInputs = othersInputs.Max();
+
+            var remaining = myInputs.Sum();
+            ulong dustThresholdPlusFee = DustThreshold + OutputFee;
+            for (int i = 50; i > 0; i--)
+            {
+                var denomPlusFee = (ulong)Math.Pow(2, i) + OutputFee;
+
+                if (denomPlusFee > largestOthersInputs)
+                {
+                    continue;
+                }
+
+                if (denomPlusFee < dustThresholdPlusFee || remaining < dustThresholdPlusFee)
+                {
+                    break;
+                }
+
+                if (denomPlusFee <= remaining)
+                {
+                    yield return denomPlusFee;
+                    remaining -= denomPlusFee;
+                }
+            }
+
+            if (remaining >= dustThresholdPlusFee)
+            {
+                yield return remaining;
+            }
+            else if (remaining == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"EUREKA! Perfect mix!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"WARNING! Dust happened: {remaining} sats");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+    }
+}
---
+++ AmountOrganization/AmountOrganization.AscendingPow2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using AmountOrganization;
+using AmountOrganization.DescPow2;
+
+var inputCount = 50;
+var userCount = 40;
+
+var randomAmounts = Sample.Amounts.GetRandomElements(inputCount);
+var groups = randomAmounts.RandomGroups(userCount).ToArray();
+
+var mixer = new DescPow2Mixer();
+var mix = (mixer as IMixer).CompleteMix(groups).Select(x => x.ToArray()).ToArray();
+
+var outputCount = mix.Sum(x => x.Count());
+var inputAmount = groups.SelectMany(x => x).Sum();
+var outputAmount = mix.SelectMany(x => x).Sum();
+var fee = inputAmount - outputAmount;
+var size = inputCount * mixer.InputSize + outputCount * mixer.OutputSize;
+var feeRate = (fee / size).ToSats();
+
+Console.WriteLine();
+Console.WriteLine($"Number of users:\t{userCount}");
+Console.WriteLine($"Number of inputs:\t{inputCount}");
+Console.WriteLine($"Number of outputs:\t{outputCount}");
+Console.WriteLine($"Total in:\t\t{inputAmount} BTC");
+Console.WriteLine($"Fee paid:\t\t{fee} BTC");
+Console.WriteLine($"Size:\t\t\t{size} vbyte");
+Console.WriteLine($"Fee rate:\t\t{feeRate} sats/vbyte");
+
+Console.WriteLine();
+
+foreach (var (value, count) in mix
+    .SelectMany(x => x).GroupBy(x => x)
+    .ToDictionary(x => x.Key, y => y.Count())
+    .Select(x => (x.Key, x.Value))
+    .OrderBy(x => x.Key))
+{
+    if (count == 1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+    }
+    Console.WriteLine($"There are {count} occurrences of\t{value} BTC output.");
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
+
+Console.ReadLine();
