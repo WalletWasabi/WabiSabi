8876ea1dd96fa0991c75f65f3001d9faeaca41ef
nopara73 <adam.ficsor73@gmail.com>
2020-12-15 11:04:30 +0100

    Add more information to display.

--- AmountOrganization/AmountOrganization.AscendingPow2/Program.cs
+++ AmountOrganization/AmountOrganization.AscendingPow2/Program.cs
@@ -5,6 +5,7 @@ using AmountOrganization.DescPow2;
 
 var inputCount = 50;
 var userCount = 40;
+var remixPercentage = 30;
 
 var preRandomAmounts = Sample.Amounts.RandomElements(inputCount);
 var preGroups = preRandomAmounts.RandomGroups(userCount);
@@ -12,31 +13,22 @@ var preGroups = preRandomAmounts.RandomGroups(userCount);
 var preMixer = new DescPow2Mixer();
 var preMix = (preMixer as IMixer).CompleteMix(preGroups);
 
-var remixCount = (inputCount / 10) * 3;
+var remixCount = (inputCount / 100) * remixPercentage;
 var randomAmounts = Sample.Amounts.RandomElements(inputCount - remixCount).Concat(preMix.SelectMany(x => x).RandomElements(remixCount));
-var groups = randomAmounts.RandomGroups(userCount).ToArray();
+var inputGroups = randomAmounts.RandomGroups(userCount).ToArray();
 var mixer = new DescPow2Mixer();
-var mix = (mixer as IMixer).CompleteMix(groups).Select(x => x.ToArray()).ToArray();
+var outputGroups = (mixer as IMixer).CompleteMix(inputGroups).Select(x => x.ToArray()).ToArray();
 
-var outputCount = mix.Sum(x => x.Count());
-var inputAmount = groups.SelectMany(x => x).Sum();
-var outputAmount = mix.SelectMany(x => x).Sum();
+var outputCount = outputGroups.Sum(x => x.Count());
+var inputAmount = inputGroups.SelectMany(x => x).Sum();
+var outputAmount = outputGroups.SelectMany(x => x).Sum();
 var fee = inputAmount - outputAmount;
 var size = inputCount * mixer.InputSize + outputCount * mixer.OutputSize;
 var feeRate = (fee / size).ToSats();
 
 Console.WriteLine();
-Console.WriteLine($"Number of users:\t{userCount}");
-Console.WriteLine($"Number of inputs:\t{inputCount}");
-Console.WriteLine($"Number of outputs:\t{outputCount}");
-Console.WriteLine($"Total in:\t\t{inputAmount} BTC");
-Console.WriteLine($"Fee paid:\t\t{fee} BTC");
-Console.WriteLine($"Size:\t\t\t{size} vbyte");
-Console.WriteLine($"Fee rate:\t\t{feeRate} sats/vbyte");
 
-Console.WriteLine();
-
-foreach (var (value, count, unique) in groups
+foreach (var (value, count, unique) in inputGroups
     .GetIndistinguishable()
     .OrderBy(x => x.value))
 {
@@ -56,7 +48,7 @@ foreach (var (value, count, unique) in groups
 
 Console.WriteLine();
 
-foreach (var (value, count, unique) in mix
+foreach (var (value, count, unique) in outputGroups
     .GetIndistinguishable()
     .OrderBy(x => x.value))
 {
@@ -73,5 +65,17 @@ foreach (var (value, count, unique) in mix
     Console.WriteLine($"There are {displayResult} occurrences of\t{value} BTC output.");
     Console.ForegroundColor = ConsoleColor.Gray;
 }
+
+Console.WriteLine();
+Console.WriteLine($"Number of users:\t{userCount}");
+Console.WriteLine($"Number of inputs:\t{inputCount}");
+Console.WriteLine($"Number of outputs:\t{outputCount}");
+Console.WriteLine($"Total in:\t\t{inputAmount} BTC");
+Console.WriteLine($"Fee paid:\t\t{fee} BTC");
+Console.WriteLine($"Size:\t\t\t{size} vbyte");
+Console.WriteLine($"Fee rate:\t\t{feeRate} sats/vbyte");
+Console.WriteLine($"Average anonset:\t{Analyzer.AverageAnonsetGain(inputGroups, outputGroups).ToString("0.##")}");
+Console.WriteLine($"Average input anonset:\t{Analyzer.AverageAnonsetGain(inputGroups).ToString("0.##")}");
+Console.WriteLine($"Average output anonset:\t{Analyzer.AverageAnonsetGain(outputGroups).ToString("0.##")}");
 
 Console.ReadLine();
---
+++ AmountOrganization/AmountOrganization/Analyzer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmountOrganization
+{
+    public static class Analyzer
+    {
+        public static decimal AverageAnonsetGain(IEnumerable<IEnumerable<decimal>> inputs, IEnumerable<IEnumerable<decimal>> outputs)
+            => AverageAnonsetGain(inputs.Select(x => x.Select(y => y.ToSats())), outputs.Select(x => x.Select(y => y.ToSats())));
+
+        public static decimal AverageAnonsetGain(IEnumerable<IEnumerable<ulong>> inputs, IEnumerable<IEnumerable<ulong>> outputs)
+        {
+            var inputSum = inputs.SelectMany(x => x).Sum();
+            var outputSum = inputs.SelectMany(x => x).Sum();
+            var ratio = (decimal)inputSum / outputSum;
+            return (AverageAnonsetGain(inputs) * ratio + AverageAnonsetGain(outputs)) / 2;
+        }
+
+        public static decimal AverageAnonsetGain(IEnumerable<IEnumerable<decimal>> valueGroups)
+            => AverageAnonsetGain(valueGroups.Select(x => x.Select(y => y.ToSats())));
+
+        public static decimal AverageAnonsetGain(IEnumerable<IEnumerable<ulong>> valueGroups)
+        {
+            var anonsetWeighted = 0ul;
+
+            foreach (var (value, count, unique) in valueGroups.GetIndistinguishable())
+            {
+                // For example if 5 BTC outputs appears 10 times,
+                // but only 9 of them are from unique users,
+                // then 8 users gained anonymity set of 10,
+                // and one user gained anonymity set of 9 for 2 coins.
+                // So this should result in 5BTC * 8 coins * 10 anonset + 5BTC * 2coins * 9anonset = 490
+                // But (1) I am lazy and the anonset gain against one person isn't as valuable
+                // than anonset gain against multiple persons, so I'm gonna go with a simple calculation
+                // that gives a lower number: 5BTC * 10 occurrence * 9 unique = 450
+                anonsetWeighted += value * (ulong)count * (ulong)unique;
+            }
+
+            ulong sum = valueGroups.SelectMany(x => x).Sum();
+            return anonsetWeighted / (decimal)sum;
+        }
+
+        public static decimal AverageAnonsetGain(IEnumerable<decimal> values)
+            => AverageAnonsetGain(values.Select(x => x.ToSats()));
+
+        /// <summary>
+        /// Calculates how much anonymity set an average amount gained in the coinjoin.
+        /// </summary>
+        /// <param name="txo"></param>
+        public static decimal AverageAnonsetGain(IEnumerable<ulong> values)
+        {
+            var anonsetWeighted = 0ul;
+
+            foreach (var (value, count) in values.GetIndistinguishable(true))
+            {
+                // For example if 5 BTC outputs appears 10 times, then this results in: 10 * 10 * 5,
+                // because 10 people gained 10 anonymity set for 5 BTC.
+                anonsetWeighted += value * (ulong)count * (ulong)count;
+            }
+
+            ulong sum = values.Sum();
+            return anonsetWeighted / (decimal)sum;
+        }
+    }
+}
